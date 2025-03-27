d4f1c069e8ab328af8d8cd4a71d9e01b20819d5e
ibrahimmn <ibrahimmn006@gmail.com>
2025-03-27 16:13:36 +0300

    PostgreSQL update. using POstgre now

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,15 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3360,
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'password',
-    entities: [__dirname + 'models/*.ts'],
-    synchronize: true,
-    logging: true
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "root",
+  password: "",
+  database: "inventory",
+  synchronize: true,
+  logging: false,
+//   entities: ["src/models/**/*.ts"],
+  entities: ["src/models/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
 });
-
-AppDataSource.initialize()
-    .then(()=> console.log("Database connected"))
-    .catch((err)=> console.error("Database connection error: ", err));
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import 'reflect-metadata';
 
 import productRoutes from './routes/product.routes';
 import invoiceRoutes from './routes/invoice.routes';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
 const port = 3000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Connected to PostgreSQL Database");
+  })
+  .catch((error) => console.error("Database connection error:", error));
 
 app.get("/", (req, res)=> {
     res.send({"message": "Server home."});
