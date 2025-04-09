e4b5185f95fb668662292c36f87aac73c1582722
designortx <designortx@gmail.com>
2025-04-09 14:12:33 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,12 @@
 import { DataSource } from "typeorm";
+import dotenv from 'dotenv';
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST || 'localhost',
   port: 5432,
-  username: "inventory_user",
-  password: "63745nil",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "inventory",
   synchronize: true,
   logging: false,
